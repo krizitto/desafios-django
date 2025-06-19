9949100b33505ea4de77d9115e91c311d79b28dc
[Chris] <[ccordova2025@alu.uct.cl]>
2025-06-19 13:52:33 -0400

    Tercer desafio django - Semana 3

--- principal/urls.py
+++ principal/urls.py
@@ -5,10 +5,14 @@
 
 from django.urls import path
 from . import views
-urlpatterns= [path('',views.saludo),
+urlpatterns= [
+    path('default/',views.default),
 
     path('saludo/<str:nombre>/', views.saludar_usuario),
     
- 
     path('numero/<int:numero>/', views.mostrar_numero),
+    
+    path('curriculum/', views.mostrar_curriculum),
+    
+    path('',views.inicio, name='inicio'),
     ]
--- principal/views.py
+++ principal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def saludo(request):
+def default(request):
     return HttpResponse("Hola bro, bienvenido")
 
 def saludar_usuario(request, nombre):
@@ -10,3 +10,10 @@ def saludar_usuario(request, nombre):
 
 def mostrar_numero(request, numero):
     return HttpResponse(f"El número es el siguiente: {numero} :p")
+
+def mostrar_curriculum(request):
+    return render(request, 'curriculum.html')
+
+def inicio(request):
+    contexto ={"nombre": "usuario"}
+    return render(request, 'index.html', contexto)
